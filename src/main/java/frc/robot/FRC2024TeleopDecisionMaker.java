480f3059dd619a2d5a85c0f63a2e02634f098f5d
Team-4585 <84157800+Team-4585@users.noreply.github.com>
2025-01-28 15:51:17 -0700

    Update FRC2024TeleopDecisionMaker.java

--- src/main/java/frc/robot/FRC2024TeleopDecisionMaker.java
+++ src/main/java/frc/robot/FRC2024TeleopDecisionMaker.java
@@ -2,11 +2,16 @@ package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.IFollower;
 
+import edu.wpi.first.wpilibj.Servo;
+
 public class FRC2024TeleopDecisionMaker {
   private FRC2024Joystick m_TheJoystick = new FRC2024Joystick();
   private FRC2024WeaponsJoystick m_TheWeaponsJoystick = new FRC2024WeaponsJoystick();
   private FRC2024Controller m_TheController = new FRC2024Controller();
   private FRC2024WeaponsController m_TheWeaponsController = new FRC2024WeaponsController();
+  Servo actuator = new Servo(0);
+  
+  boolean isOut = true;
 
   private FRC2024Chassis m_Chassis;
 
@@ -33,6 +38,14 @@ public class FRC2024TeleopDecisionMaker {
           isFieldOriented = false;
         }else{
           isFieldOriented = true;
+        }
+      }
+
+      if(m_TheJoystick.triggerPressEvent()){
+        if(isOut){
+          actuator.setAngle(50);
+        }else{
+          actuator.setAngle(130);
         }
       }
 
